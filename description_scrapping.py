99bfaca4061fd52e7585ff8f47419be5139d396e
Dov Chriqui <chriquidov@gmail.com>
2022-03-02 15:01:36 +0200

    With Links, check salary-type

--- description_scrapping.py
+++ description_scrapping.py
@@ -5,9 +5,11 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import urllib.parse
+from urllib.parse import urljoin
+
 
 TARGET_PATH = 'jobs3.csv'
-JOB_TITLE = 'Anything'
+JOB_TITLE = 'data scientist'
 LOCATION = 'United States'
 NUMBER_OF_SCRAPS = 10
 
@@ -36,8 +38,9 @@ def transform(soup, joblist):
     :param soup: soup object
     :return: soup object
     """
+    link_list=get_link_to_full_description(soup)
     divs1 = soup.find_all('div', class_='job_seen_beacon')
-    for item in divs1:
+    for i,item in enumerate(divs1):
         title = get_job_title(item)
         company = get_company_name(item)
         location = get_job_location(item)
@@ -45,11 +48,7 @@ def transform(soup, joblist):
         rating = get_company_rating(item)
         salary = get_job_salary(item)
         job_type = get_job_type(item)
-
-    divs2=soup.find_all('div' , class_='mosaic mosaic-provider-jobcards')
-    for item in divs2:
-        link_to_description = get_link_to_full_description(item)
-
+        link= link_list[i]
         job = {
             'title': title,
             'company': company,
@@ -58,7 +57,7 @@ def transform(soup, joblist):
             'salary': salary,
             'job_type': job_type,
             'summary': summary,
-            'detailed_description': link_to_description,
+            'link':link
         }
         joblist.append(job)
     return
@@ -127,19 +126,25 @@ def get_job_type(item):
         return 'Nan'
 
 
-def get_link_to_full_description(item):
-    """Param: item in page's soup
-        returns the job type of the specific job offer if exists"""
-    try:
-        return item.find('href').text.strip()
-    except:
-        return 'Nan'
+def get_link_to_full_description(soup):
+    """Param: receives the soup of the page, returns a list of the links to the job offers full description """
+    job_column = soup.find('div', class_='mosaic-provider-jobcards') #gets the column of the job offers
+    url = f'https://www.indeed.com/' #TODO: Find a way to make one url and put in as magic number!
+    link_list=[]
+    for i in job_column.find_all('a'): # loop over all the a's in the column
+        try:
+            if i.attrs['id']: # If id is provided it means the a contains info about a job offer and a relative link to the job offer
+                abs_path=urljoin(url, i.get('href')) # Gets the link from the a and creates an absolute path to the job offer detailed description
+                link_list.append(abs_path)
+        except:
+            pass
+    return link_list
 
 def main():
     # Convert Input to URL type
     job_title_url_format = urllib.parse.quote(JOB_TITLE, safe='')
     location_url_format = urllib.parse.quote(LOCATION, safe='')
-    number_of_scrap_conv = (NUMBER_OF_SCRAPS // 15 + 1) * 10
+    number_of_scrap_conv = (NUMBER_OF_SCRAPS // 15 +1 ) * 10
 
     joblist = []
     for i in range(0, number_of_scrap_conv, 10):
