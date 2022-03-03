d371ba15d38684d0e9ee31719b8677e2be66e4e1
Dov Chriqui <chriquidov@gmail.com>
2022-03-03 14:39:11 +0200

    Indeed end

--- indeed_scrapping_checkpoint_1.py
+++ indeed_scrapping_checkpoint_1.py
@@ -12,13 +12,13 @@ import conf
 Web Scraping for Indeed.com; Returns  jobs and their meta-data
 """
 
-SAMPLE_URL = f'https://www.indeed.com/'
-FILENAME = 'jobs-'
-JOB_TITLE = 'data scientist'
-LOCATION = 'United States'
-HEADERS = {
-    'User-Agent': 'Windows 10/ Edge browser: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-}
+# SAMPLE_URL = f'https://www.indeed.com/'
+# FILENAME = 'jobs-'
+# JOB_TITLE = 'data scientist'
+# LOCATION = 'United States'
+# HEADERS = {
+#     'User-Agent': 'Windows 10/ Edge browser: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+# }
 
 
 def require_number_of_scraps():
