69afc0f186e13176699e235a16bcb4619fa1c63f
Dov Chriqui <chriquidov@gmail.com>
2022-04-29 14:00:52 +0300

    clean

--- db_parameter.py
+++ db_parameter.py
@@ -1,6 +1,6 @@
 db_parameter = {
     'DATABASE_USER': 'root',
-    'DATABASE_PASSWORD': 'pass',
+    'DATABASE_PASSWORD': 'Partouche4',
     'DATABASE_PORT': '3306',
     "DATABASE_NAME": 'indeed_db',
     'DATABASE_HOST': 'localhost'
