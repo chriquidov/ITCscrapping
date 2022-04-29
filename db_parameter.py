56463f60509d49e8e4f266220541336b2885cac0
Dov Chriqui <chriquidov@gmail.com>
2022-04-29 14:44:47 +0300

    db remote

--- db_parameter.py
+++ db_parameter.py
@@ -2,7 +2,7 @@ db_parameter = {
     'DATABASE_USER': 'dov_samuel',
     'DATABASE_PASSWORD': 'dov_samuel',
     'DATABASE_PORT': '3306',
-    "DATABASE_NAME": 'indeed_db',
+    "DATABASE_NAME": 'dov_samuel',
     'DATABASE_HOST': 'data-mining-db1.cttpnp4olbpx.us-west-1.rds.amazonaws.com'
 
 }
